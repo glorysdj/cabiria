539e19cbfcf2e67616e97438194620a9c426b56e
dongjie.shi <glorysdj@gmail.com>
2014-12-11 14:07:06 +0800

    add more

--- ml-spark-MLlib/src/main/scala/me/glorysdj/cabiria/ml/spark/mllib/DTC.scala
+++ ml-spark-MLlib/src/main/scala/me/glorysdj/cabiria/ml/spark/mllib/DTC.scala
@@ -7,6 +7,9 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.configuration.Algo._
+import org.apache.spark.mllib.tree.impurity.Gini
 
 /**
  * @author dongjie.shi
@@ -14,24 +17,64 @@ import org.apache.spark.mllib.regression.LabeledPoint
  */
 object DTC {
 
-  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DecisionTreeClassification --deploy-mode client --master spark://ford-267163.phx-os1.stratus.dev.ebay.com:7077 /root/cabiria/ml-spark-mllib_2.10-0.1.jar
-  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DecisionTreeClassification --deploy-mode cluster --master yarn /root/cabiria/ml-spark-mllib_2.10-0.1.jar
+  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DTC --deploy-mode client --master spark://ford-267163.phx-os1.stratus.dev.ebay.com:7077 /root/cabiria/ml-spark-mllib_2.10-0.1.jar
+  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DTC --deploy-mode cluster --master yarn /root/cabiria/ml-spark-mllib_2.10-0.1.jar
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("DecisionTreeClassification").set("spark.executor.memory", "3g")
     val spark = new SparkContext(conf)
-    val data = spark.textFile("hdfs://ford-267163.phx-os1.stratus.dev.ebay.com:8020/user/root/input/iris/")
-    val points = data.map(_.split(",")).map(d => {
-      LabeledPoint(labelOf(d.last), Vectors.dense(d.init.map(_.toDouble)))
+    val data = spark.textFile("hdfs://ford-267163.phx-os1.stratus.dev.ebay.com:8020/user/root/input/abalone/").map(_.split(",")).cache
+
+    val errors = List.range(1, 11).map(maxDepth => {
+      val points1 = data.filter(!_.head.contains("I")).map(d => {
+        LabeledPoint(label1(d.head), Vectors.dense(d.tail.map(_.toDouble)))
+      })
+      val model1 = DecisionTree.train(points1, Classification, Gini, maxDepth)
+      val predictions = points1.map(d => {
+        //val label = d.last
+        //val features = Vectors.dense(d.init.map(_.toDouble))
+        val prediction1 = model1.predict(d.features)
+        (d.label, math.round(prediction1))
+        /*
+      prediction1 match {
+        case 1.0 => (label, "Iris-setosa")
+        case _ => {
+          val prediction2 = model2.predict(Vectors.dense(d.init.map(_.toDouble)))
+          prediction2 match {
+            case 1.0 => (label, "Iris-versicolor")
+            case _ => (label, "Iris-virginica")
+          }
+        }
+      }
+      */
+      })
+      val trainErr = predictions.filter(r => r._1 != r._2).count.toDouble / points1.count
+      (maxDepth, trainErr)
     })
-    points.cache
-    val p = points.collect
+
+    //    val points2 = data.map(d => {
+    //      LabeledPoint(label2(d.last), Vectors.dense(d.init.map(_.toDouble)))
+    //    })
+    //    val model2 = DecisionTree.train(points2, Classification, Gini, maxDepth)
+
+    errors.foreach(e => {
+      println("Training Error for (" + e._1 + "," + e._2 + ")")
+    })
 
     spark.stop()
   }
 
-  def labelOf(label: String): Double = {
-    val labels = List("Iris-setosa", "Iris-versicolor", "Iris-virginica")
-    labels.indexOf(label)
+  def label1(label: String): Double = {
+    label match {
+      case "F" => 1
+      case _ => 0
+    }
+  }
+
+  def label2(label: String): Double = {
+    label match {
+      case "M" => 1
+      case _ => 0
+    }
   }
 
   def calculateShannonEntropy(points: Array[LabeledPoint]): Double = {
--- ml-spark-MLlib/src/main/scala/me/glorysdj/cabiria/ml/spark/mllib/DecisionTreeClassification.scala
+++ ml-spark-MLlib/src/main/scala/me/glorysdj/cabiria/ml/spark/mllib/DecisionTreeClassification.scala
@@ -17,29 +17,39 @@ import org.apache.spark.mllib.tree.impurity.Gini
  */
 object DecisionTreeClassification {
 
-  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DecisionTreeClassification --deploy-mode client --master spark://ford-267163.phx-os1.stratus.dev.ebay.com:7077 /root/cabiria/ml-spark-mllib_2.10-0.1.jar
+  //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DecisionTreeClassification --deploy-mode client --master spark://ford-267163.phx-os1.stratus.dev.ebay.com:7077 /root/cabiria/ml-spark-mllib_2.10-0.1.jar hdfs://ford-267163.phx-os1.stratus.dev.ebay.com:8020/user/root/input/data/mllib/sample_tree_data.csv
   //spark-submit --class me.glorysdj.cabiria.ml.spark.mllib.DecisionTreeClassification --deploy-mode cluster --master yarn /root/cabiria/ml-spark-mllib_2.10-0.1.jar
   def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: DecisionTreeClassification <in>")
+      System.exit(1)
+    }
+    val in = args(0)
+
     val conf = new SparkConf().setAppName("DecisionTreeClassification").set("spark.executor.memory", "3g")
     val spark = new SparkContext(conf)
-    val data = spark.textFile("hdfs://ford-267163.phx-os1.stratus.dev.ebay.com:8020/user/root/input/data/mllib/sample_tree_data.csv")
+    val data = spark.textFile(in)
     val points = data.map(_.split(",")).map(d => {
       LabeledPoint(d.head.toDouble, Vectors.dense(d.tail.map(_.toDouble)))
     })
     points.cache
     // Run training algorithm to build the model
-    val maxDepth = 5
-    val model = DecisionTree.train(points, Classification, Gini, maxDepth)
+    val errors = List.range(1, 11).map(maxDepth => {
+      val model = DecisionTree.train(points, Classification, Gini, maxDepth)
 
-    // Evaluate model on training examples and compute training error
-    val labelAndPreds = points.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
-    //labelAndPreds.collect.foreach(println)
-    
-    val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / points.count
-    println("Training Error = " + trainErr)
+      // Evaluate model on training examples and compute training error
+      val labelAndPreds = points.map { point =>
+        val prediction = model.predict(point.features)
+        (point.label, prediction)
+      }
+      //labelAndPreds.collect.foreach(println)
+
+      val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / points.count
+      (maxDepth, trainErr)
+    })
+    errors.foreach(e => {
+      println("Training Error for (" + e._1 + "," + e._2 + ")")
+    })
     spark.stop()
   }
 
